76eac6a0be34fc0ed51c7c18034386e494000134
henrick168 <henrick168@gmail.com>
2016-06-12 12:56:02 +0800

    add preference

--- MyPracticeMenu/app/src/main/java/com/example/leo/mypracticemenu/MainActivity.java
+++ MyPracticeMenu/app/src/main/java/com/example/leo/mypracticemenu/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -14,62 +15,78 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "myLog";
-    Debug debug ;
+    public static final String PREF = "PROFILE";
+    public static final String PREF_NAME = "PROFILE_NAME";
+    public static final String PREF_PHONE = "PROFILE_PHONE";
+    private EditText edt_name, edt_phone;
+    Debug debug;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         debug = new Debug();
-        if(debug.On) Log.d(TAG, "onCreate");
+        if (debug.On) Log.d(TAG, "onCreate");
+        initViews();
+        restorePrefs();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        if(debug.On) Log.d(TAG, "onRestart");
+        if (debug.On) Log.d(TAG, "onRestart");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if(debug.On) Log.d(TAG, "onStart");
+        if (debug.On) Log.d(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(debug.On) Log.d(TAG, "onResume");
+        if (debug.On) Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(debug.On) Log.d(TAG, "onPause");
+        if (debug.On) Log.d(TAG, "onPause");
+
+        // Save user preference. Use Editor object to make changes.
+        SharedPreferences settings = getSharedPreferences(PREF, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(PREF_NAME, edt_name.getText().toString());
+        editor.putString(PREF_PHONE, edt_phone.getText().toString());
+        editor.commit();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if(debug.On) Log.d(TAG, "onStop");
+        if (debug.On) Log.d(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(debug.On) Log.d(TAG, "onDestroy");
+        if (debug.On) Log.d(TAG, "onDestroy");
     }
 
     protected static final int MENU_SETTING = Menu.FIRST;
 
-    // 只有在讀入Activity時會被執行一次
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // onCreateOptionsMenu() 只有在讀入Activity時會被執行一次
+
+        if (debug.On) Log.d(TAG, "onCreateOptionsMenu");
         //XML，方式定義選單
-        if(debug.On) Log.d(TAG, "onCreateOptionsMenu");
         getMenuInflater().inflate(R.menu.menu, menu);
 
         //程式碼，方式定義選單
@@ -78,15 +95,16 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    //每次執行Menu選單，會執行一次
-    /*@Override
+
+    @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
+        // onPrepareOptionsMenu() 每次執行Menu選單，會執行一次
         return super.onPrepareOptionsMenu(menu);
-    }*/
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(debug.On) Log.d(TAG, "onOptionsItemSelected");
+        if (debug.On) Log.d(TAG, "onOptionsItemSelected");
         switch (item.getItemId()) {
             case R.id.action_about:
                 setOptionsDialog();
@@ -108,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void setOptionsDialog() {
-        if(debug.On) Log.d(TAG, "setOptionsDialog");
+        if (debug.On) Log.d(TAG, "setOptionsDialog");
         // 使用匿名的實體
         // 當運行完Dialog後，系統會自行回收這個匿名實體所佔用的記憶體空間。
         new AlertDialog.Builder(this)
@@ -129,21 +147,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void setOptionsNavigation() {
-        if(debug.On) Log.d(TAG, "setOptionsNavigation");
+        if (debug.On) Log.d(TAG, "setOptionsNavigation");
         // ?z=16 , 指定縮放程度
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.uri_railway)));
         startActivity(intent);
     }
 
     protected void setOptionsDial() {
-        if(debug.On) Log.d(TAG, "setOptionsDial");
+        if (debug.On) Log.d(TAG, "setOptionsDial");
         Uri uri = Uri.parse(getString(R.string.uri_dial));
         Intent intent = new Intent(Intent.ACTION_DIAL, uri);
         startActivity(intent);
     }
 
     protected void sendNotification() {
-        if(debug.On) Log.d(TAG, "sendNotification");
+        if (debug.On) Log.d(TAG, "sendNotification");
         NotificationManager barManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
 //         改用Notification.Builder
@@ -183,5 +201,22 @@ public class MainActivity extends AppCompatActivity {
 //                "訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息訊息",
 //                contentIntent
 //        );
+    }
+
+    private void initViews() {
+        edt_name = (EditText) findViewById(R.id.editText);
+        edt_phone = (EditText) findViewById(R.id.editText2);
+    }
+
+    private void restorePrefs() {
+        // getSharedPreferences (String name, int mode)
+        // mode則是偏好設定檔作業模式
+        // MODE_PRIVATE (0)表示應用程式專用
+        // MODE_WORLD_WRITEABLE (1)表示可與套件之其它應用程式共用
+        SharedPreferences settings = getSharedPreferences(PREF, 0);
+        String pref_name = settings.getString(PREF_NAME, "");
+        String pref_phone = settings.getString(PREF_PHONE, "");
+        edt_name.setTag(pref_name);
+        edt_phone.setTag(pref_phone);
     }
 }
