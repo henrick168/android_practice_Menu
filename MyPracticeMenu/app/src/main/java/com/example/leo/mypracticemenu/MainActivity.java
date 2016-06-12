868ae2ac3f3aff408560437e7f4ae35e67f41d3f
henrick168 <henrick168@gmail.com>
2016-06-12 14:35:57 +0800

    add Pref.java (preference optimize)

--- MyPracticeMenu/app/src/main/java/com/example/leo/mypracticemenu/MainActivity.java
+++ MyPracticeMenu/app/src/main/java/com/example/leo/mypracticemenu/MainActivity.java
@@ -20,18 +20,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "myLog";
-    public static final String PREF = "PROFILE";
-    public static final String PREF_NAME = "PROFILE_NAME";
-    public static final String PREF_PHONE = "PROFILE_PHONE";
     private EditText edt_name, edt_phone;
-    Debug debug;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        debug = new Debug();
-        if (debug.On) Log.d(TAG, "onCreate");
+        if (Debug.On) Log.d(TAG, "onCreate");
         initViews();
         restorePrefs();
     }
@@ -39,46 +34,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
-        if (debug.On) Log.d(TAG, "onRestart");
+        if (Debug.On) Log.d(TAG, "onRestart");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (debug.On) Log.d(TAG, "onStart");
+        if (Debug.On) Log.d(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (debug.On) Log.d(TAG, "onResume");
+        if (Debug.On) Log.d(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (debug.On) Log.d(TAG, "onPause");
-
-
-        // Save user preference. Use Editor object to make changes.
-        SharedPreferences settings = getSharedPreferences(PREF, 0);
-        // SharedPreference Optimize
-        settings.edit()
-                .putString(PREF_NAME, edt_name.getText().toString())
-                .putString(PREF_PHONE, edt_phone.getText().toString())
-                .commit();
+        if (Debug.On) Log.d(TAG, "onPause");
+        Pref.setName(this, edt_name.getText().toString());
+        Pref.setPhone(this, edt_phone.getText().toString());
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (debug.On) Log.d(TAG, "onStop");
+        if (Debug.On) Log.d(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (debug.On) Log.d(TAG, "onDestroy");
+        if (Debug.On) Log.d(TAG, "onDestroy");
     }
 
     protected static final int MENU_SETTING = Menu.FIRST;
@@ -87,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // onCreateOptionsMenu() 只有在讀入Activity時會被執行一次
 
-        if (debug.On) Log.d(TAG, "onCreateOptionsMenu");
+        if (Debug.On) Log.d(TAG, "onCreateOptionsMenu");
         //XML，方式定義選單
         getMenuInflater().inflate(R.menu.menu, menu);
 
@@ -106,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (debug.On) Log.d(TAG, "onOptionsItemSelected");
+        if (Debug.On) Log.d(TAG, "onOptionsItemSelected");
         switch (item.getItemId()) {
             case R.id.action_about:
                 setOptionsDialog();
@@ -128,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void setOptionsDialog() {
-        if (debug.On) Log.d(TAG, "setOptionsDialog");
+        if (Debug.On) Log.d(TAG, "setOptionsDialog");
         // 使用匿名的實體
         // 當運行完Dialog後，系統會自行回收這個匿名實體所佔用的記憶體空間。
         new AlertDialog.Builder(this)
@@ -149,21 +137,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void setOptionsNavigation() {
-        if (debug.On) Log.d(TAG, "setOptionsNavigation");
+        if (Debug.On) Log.d(TAG, "setOptionsNavigation");
         // ?z=16 , 指定縮放程度
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.uri_railway)));
         startActivity(intent);
     }
 
     protected void setOptionsDial() {
-        if (debug.On) Log.d(TAG, "setOptionsDial");
+        if (Debug.On) Log.d(TAG, "setOptionsDial");
         Uri uri = Uri.parse(getString(R.string.uri_dial));
         Intent intent = new Intent(Intent.ACTION_DIAL, uri);
         startActivity(intent);
     }
 
     protected void sendNotification() {
-        if (debug.On) Log.d(TAG, "sendNotification");
+        if (Debug.On) Log.d(TAG, "sendNotification");
         NotificationManager barManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
 //         改用Notification.Builder
@@ -215,10 +203,11 @@ public class MainActivity extends AppCompatActivity {
         // mode則是偏好設定檔作業模式
         // MODE_PRIVATE (0)表示應用程式專用
         // MODE_WORLD_WRITEABLE (1)表示可與套件之其它應用程式共用
-        SharedPreferences settings = getSharedPreferences(PREF, 0);
-        String pref_name = settings.getString(PREF_NAME, "");
-        String pref_phone = settings.getString(PREF_PHONE, "");
+        String pref_name = Pref.getName(this);
+        String pref_phone = Pref.getPhone(this);
         edt_name.setText(pref_name);
         edt_phone.setText(pref_phone);
+        if (!"".equals(pref_name)) edt_phone.requestFocus();
+        if (!"".equals(pref_phone)) edt_name.requestFocus();
     }
 }
---
+++ MyPracticeMenu/app/src/main/java/com/example/leo/mypracticemenu/Pref.java
@@ -0,0 +1,36 @@
+package com.example.leo.mypracticemenu;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Leo on 2016/6/12.
+ */
+public class Pref extends PreferenceActivity {
+    public static final String PREF = "PROFILE";
+    public static final String PREF_NAME = "PROFILE_NAME";
+    public static final String PREF_PHONE = "PROFILE_PHONE";
+
+    public static String getName(Context context) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        return pref.getString(PREF_NAME, "");
+    }
+
+    public static String getPhone(Context context) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        return pref.getString(PREF_PHONE, "");
+    }
+
+    public static void setName(Context context, String name) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        pref.edit().putString(PREF_NAME, name).commit();
+    }
+
+    public static void setPhone(Context context, String phone) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        pref.edit().putString(PREF_PHONE, phone).commit();
+    }
+
+}
